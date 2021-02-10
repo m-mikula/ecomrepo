ba6c00ab472e2022118dc94474e1ec173e4ef79b
m-mikula <martin.mikula83@gmail.com>
2021-02-10 20:10:05 +0000

    Deploying to gh-pages from master @ a7b5649861df32cd42e1fb039ea1db39eb827f67 🚀

--- pipo.tsx
+++ pipo.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="2021.01.13" name="pipo" tilewidth="32" tileheight="32" tilecount="1072" columns="8">
- <image source="_pipo.png" width="256" height="4288"/>
+<tileset version="1.4" tiledversion="1.4.3" name="pipo" tilewidth="32" tileheight="32" tilecount="1064" columns="8">
+ <image source="[Base]BaseChip_pipo.png" width="256" height="4256"/>
 </tileset>
